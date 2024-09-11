472843b575362496a9cbaa893665761ea743ae61
Hengkang Qiao <2468439195@qq.com>
2024-09-11 10:57:51 +0800

    引入markdown包，编写完成git命令查询

--- components/block/block.jsx
+++ components/block/block.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./block.css";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm"; // 引入插件
 
-function Block({target,content}) {
+function Block({ target, content }) {
+  const markdowncontent = `
+  ### ${content}
+  `;
+
   return (
     <NavLink to={target} className="block">
-      {content}
+      <Markdown>{markdowncontent}</Markdown>
     </NavLink>
-  )
+  );
 }
 
 export default Block;
--- pages/gitpage/gitpage.jsx
+++ pages/gitpage/gitpage.jsx
@@ -1,7 +1,46 @@
-import "./gitpage.css"
+import React from "react";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm"; // 引入插件
+import "./gitpage.css";
 
 function Gitpage() {
-    return <div className="content">这里是git命令栏查询</div>
+  const content = `
+  # Git 命令查询
+  ### Git 配置用户信息
+  \`\`\`
+  git config --global user.name "[name]"
+  git config --global user.email "[email]"
+  \`\`\`
+  ---
+  ### Git 初始化仓库
+  \`\`\`
+  git init
+  \`\`\`
+  ---
+  ### Git 添加文件到暂存区
+  \`\`\`
+  git add [file]
+  \`\`\`
+  ---
+  ### Git 提交文件到仓库
+  \`\`\`
+  git commit -m "[message]"
+  \`\`\`
+  ---
+  ### Git 克隆远程仓库
+  \`\`\`
+  git clone [url]
+  \`\`\`
+  ---
+  `;
+
+  return (
+    <div className="contents">
+      <Markdown className="center" remarkPlugins={[remarkGfm]}>
+        {content}
+      </Markdown>
+    </div>
+  );
 }
 
-export default Gitpage
+export default Gitpage;
