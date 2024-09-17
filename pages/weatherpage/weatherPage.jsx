3b28afdd8af6f8dcaaf75f0f7195f590c24c91ad
Hengkang Qiao <2468439195@qq.com>
2024-09-17 14:24:19 +0800

    添加loading和error处理

--- pages/weatherpage/weatherPage.jsx
+++ pages/weatherpage/weatherPage.jsx
@@ -2,42 +2,86 @@ import { useState } from "react";
 import "./weatherPage.css";
 
 function WeatherPage() {
-    const [weather, setWeather] = useState({
-        location: {
-            name: ""
-        },
-        current: {
-            temperature: "",
-            weather_descriptions: "",
-            humidity: ""
-        }
-    });
+  const [isLoading, setisLoading] = useState(false);
 
+  const [weather, setWeather] = useState({
+    location: {
+      name: "",
+    },
+    current: {
+      temperature: "",
+      weather_descriptions: "",
+      humidity: "",
+    },
+  });
 
-    async function fetchWeather(cityName) {
-        const res = await fetch(`http://api.weatherstack.com/current?access_key=f4eca985bc0abda26f264fe47efbfd3b&query=${cityName}`);
-        const data = await res.json();
-        setWeather(data);
-        console.log(data);
+  const [error, setError] = useState("");
+
+  async function fetchWeather(cityName) {
+    try {
+      setisLoading(true);
+      const res = await fetch(
+        `http://api.weatherstack.com/current?access_key=f4eca985bc0abda26f264fe47efbfd3b&query=${cityName}`
+      );
+
+      if (!res.ok) {
+        throw new Error("Something went wrong with fetching weather");
+      }
+
+      const data = await res.json();
+      setWeather(data);
+      console.log(data);
+    } catch (err) {
+      console.error(err.message);
+      setError(err.message);
+    } finally {
+      setisLoading(false);
     }
+  }
 
-    function handleClick() {
-        const cityInput = document.getElementById("weather").value;
-        fetchWeather(cityInput);  // 直接使用输入框的值来查询天气
-    }
+  function handleClick() {
+    const cityInput = document.getElementById("weather").value;
+    fetchWeather(cityInput); // 直接使用输入框的值来查询天气
+  }
 
+  function Loader() {
     return (
-        <div className="contents">
-            <h1>查询天气</h1>
-            <input type="text" id="weather" />
-            <button onClick={handleClick}>查询</button>
-            <h3>城市: {weather.location.name}</h3>
-            <h3>温度: {weather.current.temperature}</h3>
-            <h3>天气: {weather.current.weather_descriptions}</h3>
-            <img src={weather.current.weather_icons[0]} alt=""/>
-            <h3>湿度: {weather.current.humidity}</h3>
-        </div>
+      <div>
+        <h2>Loading...</h2>
+      </div>
     );
+  }
+
+  function ErrorMessage({ message }) {
+    return (
+      <div>
+        <p>
+          <span>👻</span>
+          {message}
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="contents">
+      <h1>查询天气</h1>
+      <input type="text" id="weather" />
+      <button onClick={handleClick}>查询</button>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <ErrorMessage message={error} />
+      ) : (
+        <div>
+          <h3>城市: {weather.location.name}</h3>
+          <h3>温度: {weather.current.temperature}</h3>
+          <h3>天气: {weather.current.weather_descriptions}</h3>
+          <h3>湿度: {weather.current.humidity}</h3>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default WeatherPage;
